e37eec567ee5ea41d1cef20083b1111021f0c51e
mbuc1c <you@example.com>
2023-06-23 10:42:34 +0200

    managed packages

--- app/src/main/java/com/example/freegamesapp/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/freegamesapp/presentation/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.freegamesapp
+package com.example.freegamesapp.presentation.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.freegamesapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
